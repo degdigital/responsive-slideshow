6109a3c20d5da3e493a1f112874b490e4e39e9a6
Deric Mendez <mendezd@digitalev.com>
2013-02-28 16:37:06 -0600

    reorganize slides, add loading icon

--- js/app.js
+++ js/app.js
@@ -6,9 +6,14 @@
 		prev: "left",
 		next: "right",
         circular: false,
+        infinite: false,
 		pagination: '.presentation-nav',
         cookie: 'true',
 		scroll : { wipe: true }
 	});
+
+    $(window).load(function() {
+        $('body').addClass('loaded');        
+    });
 
 })(jQuery);
